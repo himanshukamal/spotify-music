bea44a7cc04a7ff8de4fd245518f78849524de08
unknown <himanshukamal>
2021-03-21 11:35:28 +0530

    import os

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import os
 
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.getenv('CLIENT_ID',
-	client_secret=os.getenv('CLIENT_SECRET')))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+	client_id=os.getenv('CLIENT_ID'),
+	client_secret=os.getenv('CLIENT_SECRET')
+	)
+)
 
 
 
